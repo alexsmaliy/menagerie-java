794729c0ea24e7396c5b3f4b266a39c09e71f77c
Alex Smaliy <alex.smaliy@gmail.com>
2019-09-17 18:55:41 -0700

    finish graph

--- src/main/java/adm2e/graph/representations/AdjacencyListGraph.java
+++ src/main/java/adm2e/graph/representations/AdjacencyListGraph.java
@@ -1,14 +1,16 @@
 package adm2e.graph.representations;
 
+import gnu.trove.TIntCollection;
 import gnu.trove.list.array.TIntArrayList;
 import gnu.trove.map.hash.TIntObjectHashMap;
 import gnu.trove.map.hash.TObjectIntHashMap;
+import gnu.trove.set.hash.TIntHashSet;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class AdjacencyListGraph {
+public class AdjacencyListGraph implements DirectedGraph {
     private final List<TIntArrayList> edges;
     private final TIntObjectHashMap<String> keyToLabel;
     private final TObjectIntHashMap<String> labelToKey;
@@ -19,13 +21,14 @@ public class AdjacencyListGraph {
         this.labelToKey = new TObjectIntHashMap<>();
     }
 
+    @Override
     public void addEdge(String from, String to, boolean directed) {
         maybeInitializeLabel(from);
         maybeInitializeLabel(to);
         int keyOfFrom = labelToKey.get(from);
         int keyOfTo = labelToKey.get(to);
         edges.get(keyOfFrom).add(keyOfTo);
-        if (directed) edges.get(keyOfTo).add(keyOfFrom);
+        if (!directed) edges.get(keyOfTo).add(keyOfFrom);
     }
 
     private void maybeInitializeLabel(String label) {
@@ -36,12 +39,20 @@ public class AdjacencyListGraph {
         }
     }
 
+    @Override
     public int numVertices() {
         return edges.size();
     }
 
+    @Override
     public Optional<String> labelOfVertex(int vertex) {
-        if (vertex < 0 || vertex >= numVertices()) return Optional.empty();
-        return Optional.of(keyToLabel.get(vertex));
+        return Optional.ofNullable(keyToLabel.get(vertex));
+    }
+
+    @Override
+    public TIntCollection childrenOfVertex(int vertex, boolean deduplicateEdges) {
+        if (vertex < 0 || vertex >= numVertices()) return new TIntArrayList();
+        if (deduplicateEdges) return new TIntHashSet(edges.get(vertex));
+        else return new TIntArrayList(edges.get(vertex));
     }
 }
