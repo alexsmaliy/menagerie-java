a1f5dc57491718c567ddb3f6ad17c2b67ad8c693
Alex Smaliy <alex.smaliy@gmail.com>
2019-10-04 18:54:35 -0700

    inline comments for trie

--- src/main/java/ads/trie/DictionaryReductionTrie.java
+++ src/main/java/ads/trie/DictionaryReductionTrie.java
@@ -4,8 +4,8 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Deque;
 import java.util.List;
+import java.util.Queue;
 
 public final class DictionaryReductionTrie {
 
@@ -85,6 +85,8 @@ public final class DictionaryReductionTrie {
         return true;
     }
 
+    // Return `true` iff an exact match has been inserted into the trie.
+    // Namely, we check if `s + '\0'` is in the trie.
     public boolean checkExactMatch(String s) {
         return checkInternal(reduce(s), 0);
     }
@@ -98,7 +100,7 @@ public final class DictionaryReductionTrie {
         boolean[] b = reduce(s);
         DictionaryReductionTrie trie = this;
         // Descend to the root node of all suffixes,
-        // or return empty result if prefix is not found in trie.
+        // or return empty result if the prefix is not found in the trie.
         for (int i = 0; i < b.length - 8; i++) {
             if (b[i] && trie.t != null) {
                 trie = trie.t;
@@ -108,42 +110,70 @@ public final class DictionaryReductionTrie {
                 return Collections.emptyList();
             }
         }
+
+        // DFS to get all suffixes from the node we reached.
         List<String> ret = new ArrayList<>();
         boolean[][] suffixes = dfs(trie);
+
+        // Translate all the suffixes into strings.
         for (int i = 0; i < suffixes.length; i++) {
             ret.add(s + unreduce(suffixes[i]));
         }
         return ret;
     }
 
+    // Generate all suffixes of a node using simple DFS using a LIFO queue.
+    // Since node values are not distinct (namely, only true and false),
+    // we store lists of the nodes themselves as we traverse them. Lists
+    // are ultimately translated into boolean arrays, and then remapped
+    // back to strings by the caller.
     private static boolean[][] dfs(DictionaryReductionTrie trie) {
-        Deque<List<DictionaryReductionTrie>> dq = new ArrayDeque<>();
+        Queue<List<DictionaryReductionTrie>> q = new ArrayDeque<>();
         List<List<DictionaryReductionTrie>> suffixes = new ArrayList<>();
+
+        // We start the queue with the root node that the caller provided.
+        // We do not include the root's boolean value as part of the return.
+        // We only use it to traverse each suffix to determine whether each
+        // node is its parent's `t` or `f` child.
         List<DictionaryReductionTrie> root = new ArrayList<>();
         root.add(trie);
-        dq.add(root);
-        while (!dq.isEmpty()) {
-            List<DictionaryReductionTrie> candidate = dq.pollFirst();
+        q.add(root);
+
+        // DFS itself.
+        while (!q.isEmpty()) {
+            List<DictionaryReductionTrie> candidate = q.poll();
+
+            // Get the last noe in the partial suffix.
             DictionaryReductionTrie t = candidate.get(candidate.size() - 1);
+            // The suffix is complete if its last node has no children.
+            // Add it to the list of suffixes and add nothing back into the queue.
             if (t.t == null && t.f == null) {
                 suffixes.add(candidate);
                 continue;
             }
+            // If there is a `t` child, extend the suffix and add it back to the queue.
             if (t.t != null) {
                 List<DictionaryReductionTrie> newCandidate = new ArrayList<>(candidate);
                 newCandidate.add(t.t);
-                dq.add(newCandidate);
+                q.add(newCandidate);
             }
+            // Same for the scenario when there is an `f` child.
             if (t.f != null) {
                 List<DictionaryReductionTrie> newCandidate = new ArrayList<>(candidate);
                 newCandidate.add(t.f);
-                dq.add(newCandidate);
+                q.add(newCandidate);
             }
         }
+
+        // Traverse each suffix to translate it into a boolean array.
         boolean[][] ret = new boolean[suffixes.size()][];
         for (int i = 0; i < suffixes.size(); i++) {
             List<DictionaryReductionTrie> suffix = suffixes.get(i);
+            // The boolean array for the ith suffix is everything
+            // in the suffix, except the root that every suffix shares.
             boolean[] ith = new boolean[suffix.size() - 1];
+            // Iterate backwards through each node in the suffix,
+            // checking if it is its predecessor's `t` or `f` child.
             for (int j = suffix.size() - 1; j > 0; j--) {
                 if (suffix.get(j) == suffix.get(j - 1).t) {
                     ith[j - 1] = true;
@@ -156,6 +186,8 @@ public final class DictionaryReductionTrie {
         return ret;
     }
 
+    // A convenience method to check whether `boolean[] b` is in the trie,
+    // up to a prefix of some ignored length.
     private boolean checkInternal(boolean[] b, int ignoredSuffixLength) {
         DictionaryReductionTrie trie = this;
         for (int i = 0; i < b.length - ignoredSuffixLength; i++) {
@@ -172,6 +204,7 @@ public final class DictionaryReductionTrie {
 
     private static boolean[] reduce(String s) {
         boolean[] ret = new boolean[s.length() * 8 + 8];
+        // Translate each char to 8 booleans.
         for (int i = 0; i < s.length(); i++) {
             char si = s.charAt(i);
             ret[i * 8    ] = (si &   1) != 0;
@@ -197,11 +230,11 @@ public final class DictionaryReductionTrie {
 
     private static String unreduce(boolean[] b) {
         int numChars = b.length >> 3;
-        // Ignore the final `\0` char.
+        // Ignore the final `\0` char that `reduce()` introduces.
         char[] c = new char[numChars - 1];
         for (int i = 0; i < numChars - 1; i++) {
             c[i] = (char) (
-                (b[i * 8] ? 1 : 0)
+                  (b[i * 8    ] ?   1 : 0)
                 + (b[i * 8 + 1] ?   2 : 0)
                 + (b[i * 8 + 2] ?   4 : 0)
                 + (b[i * 8 + 3] ?   8 : 0)
